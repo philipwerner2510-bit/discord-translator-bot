c91cb67cec0ead7c0675e35fef8826ff9635f195
philipwerner2510-bit <philipwerner2510@gmail.com>
2025-11-05 17:06:56 +0100

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -1,26 +1,21 @@
-# cogs/events.py
 import discord
 from discord.ext import commands
 
 class Events(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.processing_reactions = set()
+    def __init__(self, bot): self.bot = bot
+    processing = set()
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        if user.bot:
-            return
+        if user.bot: return
         key = (reaction.message.id, user.id)
-        if key in self.processing_reactions:
-            return
-        self.processing_reactions.add(key)
+        if key in self.processing: return
+        self.processing.add(key)
         try:
             cog = self.bot.get_cog("Translate")
             if cog:
                 await cog.on_reaction_add(reaction, user)
         finally:
-            self.processing_reactions.discard(key)
+            self.processing.discard(key)
 
-async def setup(bot):
-    await bot.add_cog(Events(bot))
+async def setup(bot): await bot.add_cog(Events(bot))
